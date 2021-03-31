ea61e05bb03842af15b5137b4d53f3fc751c9bf4
Rose Williams <rosew@binghamton.edu>
2021-03-31 02:43:40 +0000

    welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  
+  get 'welcome/home', :as => :welcome_home
+  get 'welcome/about', :as => :welcome_about
+  root 'welcome#home'
+  
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
